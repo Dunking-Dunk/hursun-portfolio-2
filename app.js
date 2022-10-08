696ea5aa024839f811f7ed2b7dbbb30e2c55069f
hursun <hursunss@gmail.com>
2022-10-08 22:57:17 +0530

    first commit

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(methodOverride())
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
-
 app.use(express.static(path.join(__dirname, 'public')))
 
 const client = prismic.createClient(process.env.PRISMIC_REPO_NAME, {
